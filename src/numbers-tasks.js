cc97ec3dff87975a58832fbc5661ec1b172f6fc8
tpmail <workingtrg@gmail.com>
2026-09-21 17:34:25 +0300

    feat: add getAverage and getDistanceBetweenPoints

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -49,8 +49,10 @@ function getCircleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  if (value1 === Number.MAX_VALUE && value2 === Number.MAX_VALUE)
+    return Number.MAX_VALUE;
+  return (value1 + value2) / 2;
 }
 
 /**
@@ -67,8 +69,8 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
 /**
